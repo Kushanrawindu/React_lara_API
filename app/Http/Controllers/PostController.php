ddb3089221429bd1ed6381fadfbbf6e75dec4f65
Kushanrawindu <kushanrawindu@gmail.com>
2020-05-18 12:59:02 +0530

    create post contrller functions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
-    //
+    public function get()
+    {
+        return Post::get();
+    }
+
+    public function delete($id)
+    {
+        return Post::destroy($id);
+    }
+
+    public function put(Request $request, $id)
+    {
+        $post = Post::get($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+        return response()->json($post);
+    }
+
+    public function post(Request $request)
+    {
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+        return response()->json($post);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/', 'PostController@get');
+Route::get('/{id}', 'PostController@delete');
+Route::get('/{id}', 'PostController@put');
+Route::get('/', 'PostController@post');
